f19c6d7c6b0312d5439e21843e359cad9e3b4cf9
Francisco Souza <f@souza.cc>
2011-10-01 18:46:58 -0300

    Starting to create the structure

--- generate_talk.go
+++ generate_talk.go
@@ -1,4 +1,33 @@
 package main
 
+import (
+	"flag"
+	"os"
+	"path"
+)
+
+func copyDir(dst, src string) {
+	// FIXME: need to copy from a directory to another
+}
+
+func generatePresentation(name, theme string) {
+	if dir, err := os.Getwd(); err == nil {
+		pDir := path.Join(dir, "out", name)
+		tDir := path.Join(dir, "themes", theme)
+		copyDir(path.Join(pDir, "theme"), tDir)
+
+		os.MkdirAll(pDir, 0755)
+	}
+}
+
 func main() {
+	name := flag.String("name", "", "The presentation name (required)")
+	theme := flag.String("theme", "default", "The theme to be used (available in the themes directory)")
+	flag.Parse()
+
+	if *name == "" {
+		flag.Usage()
+	} else {
+		generatePresentation(*name, *theme)
+	}
 }
